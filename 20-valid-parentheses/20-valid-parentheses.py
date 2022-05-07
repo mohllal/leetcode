3ca6bcaabe42cc7489f5fe9ad3dc4d92836afe64
Kareem Khaled <kareem.mohllal@gmail.com>
2022-05-07 18:32:04 +0200

    Time: 45 ms (46.10%), Space: 14 MB (25.71%) - LeetHub

---
+++ 20-valid-parentheses/20-valid-parentheses.py
@@ -0,0 +1,19 @@
+class Solution:
+    def isValid(self, s: str) -> bool:
+        openings = '({[';
+        pairs = { '(': ')', '{': '}', '[': ']' };
+        stack = [];
+        for char in s:
+            if char in openings:
+                stack.append(char);
+            else:
+                if not stack:
+                    return False;
+
+                last = stack.pop();
+                if pairs[last] != char:
+                    return False;
+        if stack:
+            return False;
+        else:
+            return True
