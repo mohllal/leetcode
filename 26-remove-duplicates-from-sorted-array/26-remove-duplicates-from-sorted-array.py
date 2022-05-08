0dd53974aef4945593d471af8c123a83e4404eeb
Kareem Khaled <kareem.mohllal@gmail.com>
2022-05-08 21:00:57 +0200

    Time: 129 ms (49.80%), Space: 15.7 MB (17.16%) - LeetHub

--- 26-remove-duplicates-from-sorted-array/26-remove-duplicates-from-sorted-array.py
+++ 26-remove-duplicates-from-sorted-array/26-remove-duplicates-from-sorted-array.py
@@ -3,9 +3,7 @@ class Solution:
         i = 0
         j = 0
 
-        while i < len(nums):
-            if j >= len(nums):
-                break
+        while j < len(nums):
             if nums[i] != nums[j]:
                 i += 1
                 nums[i], nums[j] = nums[j], nums[i]
