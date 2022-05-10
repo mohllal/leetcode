a009f7fa821cccddeba812236aca69c684ee3101
Kareem Khaled <kareem.mohllal@gmail.com>
2022-05-10 23:44:47 +0200

    Time: 45 ms (70.83%), Space: 13.8 MB (57.92%) - LeetHub

--- 69-sqrtx/69-sqrtx.py
+++ 69-sqrtx/69-sqrtx.py
@@ -1,22 +1,20 @@
-def binarySearch(low, high, squares, target):
+def binarySearch(low, high, target):
     if high < low:
         return (low, False)
 
     mid = (low + high) // 2
-    if squares[mid] > target:
-        return binarySearch(low, mid - 1, squares, target)
-    elif squares[mid] < target:
-        return binarySearch(mid + 1, high, squares, target)
+    current = mid * mid
+
+    if current > target:
+        return binarySearch(low, mid - 1, target)
+    elif current < target:
+        return binarySearch(mid + 1, high, target)
     else:
         return (mid, True)
     
 class Solution:
     def mySqrt(self, x: int) -> int:
         roots = 46341 # square root of 2^31
-        squares = [0] * roots
-        
-        for i in range(0, roots):
-            squares[i] = i * i
 
-        (index, match) = binarySearch(0, roots - 1, squares, x)
+        (index, match) = binarySearch(0, roots - 1, x)
         return index - 1 if (not match) else index
