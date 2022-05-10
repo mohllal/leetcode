b2035a583c53bae94a3f4b2cdb38cd7e0f102d44
Kareem Khaled <kareem.mohllal@gmail.com>
2022-05-10 22:31:34 +0200

    Time: 33 ms (86.06%), Space: 14 MB (12.17%) - LeetHub

---
+++ 66-plus-one/66-plus-one.py
@@ -0,0 +1,22 @@
+class Solution:
+    def plusOne(self, digits: List[int]) -> List[int]:
+        result = []
+        remaining = 1
+        i = len(digits) - 1
+
+        while i >= 0:
+            current = digits[i] + remaining
+            if current == 10:
+                result = [0] + result
+                remaining = 1
+            else:
+                result = [current] + result
+                remaining = 0
+            i -= 1
+        
+        if remaining == 1:
+            result = [1] + result
+
+        return result
+            
+            
