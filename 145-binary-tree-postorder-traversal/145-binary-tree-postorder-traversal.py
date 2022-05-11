b76718ada6b5a8551a8427d8630a01c28f58335e
Kareem Khaled <kareem.mohllal@gmail.com>
2022-05-12 01:54:20 +0200

    Time: 27 ms (95.35%), Space: 13.9 MB (63.16%) - LeetHub

---
+++ 145-binary-tree-postorder-traversal/145-binary-tree-postorder-traversal.py
@@ -0,0 +1,24 @@
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+class Solution:
+    def postorderTraversal(self, root: Optional[TreeNode]) -> List[int]:
+        if root is None:
+            return []
+
+        postorder = []
+        stack = [root]
+
+        while len(stack) > 0:
+            node = stack.pop()
+            postorder.append(node.val)
+            if node.left:
+                stack.append(node.left)
+            if node.right:
+                stack.append(node.right)
+
+        postorder.reverse()
+        return postorder
