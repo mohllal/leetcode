3822043f67ce7b18101199f9ca4dc00f8a95fe6a
Kareem Khaled <kareem.mohllal@gmail.com>
2022-05-12 00:34:49 +0200

    Time: 48 ms (33.10%), Space: 13.9 MB (60.71%) - LeetHub

---
+++ 144-binary-tree-preorder-traversal/144-binary-tree-preorder-traversal.py
@@ -0,0 +1,23 @@
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+class Solution:
+    def preorderTraversal(self, root: Optional[TreeNode]) -> List[int]:
+        if root is None:
+            return []
+
+        preorder = []
+        stack = [root]
+
+        while len(stack) > 0:
+            node = stack.pop()
+            preorder.append(node.val)
+            if node.right:
+                stack.append(node.right)
+            if node.left:
+                stack.append(node.left)
+
+        return preorder            
