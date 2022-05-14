f5a5cfe6858f0766d50e3596c078e9b33828de68
Kareem Khaled <kareem.mohllal@gmail.com>
2022-05-14 22:06:18 +0200

    Time: 53 ms (22.14%), Space: 13.9 MB (75.46%) - LeetHub

--- 100-same-tree/100-same-tree.py
+++ 100-same-tree/100-same-tree.py
@@ -5,20 +5,35 @@ class TreeNode:
         self.right = right
 
 class Solution:
-    def isSameTree(self, p: Optional[TreeNode], q: Optional[TreeNode]) -> bool:
+    def isSameTreeRecursive(self, left: Optional[TreeNode], right: Optional[TreeNode]) -> bool:
+        if (left is None and right is not None) or (right is None and left is not None):
+                return False
+        
+        if (left is not None and right is not None) and left.val != right.val:
+                return False
+
+        if left is None and right is None:
+                return True
+
+        isFirstHalfSame = self.isSameTreeRecursive(left.left, right.left)
+        isSecondHalfSame = self.isSameTreeRecursive(left.right, right.right)
+        
+        return isFirstHalfSame and isSecondHalfSame
+ 
+    def isSameTreeIterative(self, p: Optional[TreeNode], q: Optional[TreeNode]) -> bool:
         pQueue = [p]
         qQueue = [q]
-        
+
         while len(pQueue) > 0 or len(qQueue) > 0:
             if len(pQueue) == 0:
                 return False
-            
+
             if len(qQueue) == 0:
                 return False
-            
+
             pNode = pQueue.pop(0)
             qNode = qQueue.pop(0)
-            
+
             if (pNode is None and qNode is not None) or (qNode is None and pNode is not None):
                 return False
 
@@ -26,12 +41,15 @@ class Solution:
                 return False
 
             if qNode is None and pNode is None:
-                continue  
+                continue
 
             pQueue.append(pNode.left)
             pQueue.append(pNode.right)
-            
+
             qQueue.append(qNode.left)
             qQueue.append(qNode.right)
-            
+
         return True
+
+    def isSameTree(self, p: Optional[TreeNode], q: Optional[TreeNode]) -> bool:
+        return self.isSameTreeRecursive(p, q)
