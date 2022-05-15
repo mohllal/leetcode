e1a6f63d10705e8295aeb684a135777cb44db017
Kareem Khaled <kareem.mohllal@gmail.com>
2022-05-15 15:27:38 +0200

    Time: 41 ms (65.82%), Space: 14 MB (25.07%) - LeetHub

---
+++ 67-add-binary/67-add-binary.py
@@ -0,0 +1,32 @@
+class Solution:
+    def add(self, x: int, y: int, z: int):
+        arr = [x, y, z]
+        ones = arr.count(1)
+        
+        if ones == 0:
+            return 0, 0
+        elif ones == 1:
+            return 1, 0
+        elif ones == 2:
+            return 0, 1
+        else:
+            return 1, 1
+
+    def addBinary(self, a: str, b: str) -> str:
+        i = len(a) - 1
+        j = len(b) - 1
+        
+        result = ''
+        carry = 0
+
+        while i >= 0 or j >= 0:
+            aBit = int(a[i]) if i >= 0 else 0
+            bBit = int(b[j]) if j >= 0 else 0
+
+            bit, carry = self.add(aBit, bBit, carry)
+
+            result = str(bit) + result
+            i -= 1
+            j -= 1
+
+        return result if carry == 0 else str(carry) + result
