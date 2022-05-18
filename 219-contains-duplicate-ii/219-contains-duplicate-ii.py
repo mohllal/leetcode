357e919ea2420f69b93a0f228bbf7c708653afb0
Kareem Khaled <kareem.mohllal@gmail.com>
2022-05-18 23:36:58 +0200

    Time: 1030 ms (15.99%), Space: 30.9 MB (14.12%) - LeetHub

--- 219-contains-duplicate-ii/219-contains-duplicate-ii.py
+++ 219-contains-duplicate-ii/219-contains-duplicate-ii.py
@@ -1,5 +1,5 @@
 class Solution:
-    def containsNearbyDuplicate(self, nums: List[int], k: int) -> bool:
+    def containsNearbyDuplicateSlower(self, nums: List[int], k: int) -> bool:
         hashtable = {}
         
         for i in range(0, len(nums)):
@@ -11,5 +11,20 @@ class Solution:
                 hashtable[nums[i]].append(i)
             else:
                 hashtable[nums[i]] = [i]
+
+        return False
+
+    def containsNearbyDuplicate(self, nums: List[int], k: int) -> bool:
+        for i in range(0, len(nums)):
+            nums[i] = (nums[i], i)
+        
+        nums.sort(key=lambda num:num[0])
+        
+        val, index = nums[0]
+        for i in range(1, len(nums)):
+            current, j = nums[i]
+            if current == val and abs(index - j) <= k:
+                return True
+            val, index = nums[i]
 
         return False
