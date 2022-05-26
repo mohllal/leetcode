5f8847d90de9d873b0925e9e17a2c87aed734adf
Kareem Khaled <kareem.mohllal@gmail.com>
2022-05-26 17:19:52 +0200

    Time: 246 ms, Memory: 15 MB - LeetHub

--- two-sum-ii-input-array-is-sorted/two-sum-ii-input-array-is-sorted.py
+++ two-sum-ii-input-array-is-sorted/two-sum-ii-input-array-is-sorted.py
@@ -33,14 +33,19 @@ class Solution:
             j = self.binarySearchRecursive(i + 1, len(numbers) - 1, numbers, remaining)
             if j != -1:
                 return [i + 1, j + 1]
-    
-    # O(n * log n) time and O(n) space
-    def twoSumLinearithmicTimeAndLinearSpace(self, numbers: List[int], target: int) -> List[int]:
-        for i in range(0, len(numbers)):
-            remaining = target - numbers[i]
-            j = self.binarySearchIterative(i + 1, len(numbers) - 1, numbers, remaining)
-            if j != -1:
+
+    # O(n) time and O(1) space
+    def twoSumLinearTimeAndConstantSpace(self, numbers: List[int], target: int) -> List[int]:
+        i = 0
+        j = len(numbers) - 1
+        
+        while i < j:
+            if (numbers[i] + numbers[j]) == target:
                 return [i + 1, j + 1]
+            elif (numbers[i] + numbers[j]) > target:
+                j -= 1
+            else:
+                i += 1
 
     def twoSum(self, numbers: List[int], target: int) -> List[int]:
-        return self.twoSumLinearithmicTimeAndLinearSpace(numbers, target)
+        return self.twoSumLinearTimeAndConstantSpace(numbers, target)
