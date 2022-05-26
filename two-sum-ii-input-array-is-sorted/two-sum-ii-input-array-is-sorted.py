755cd3e129a8bf31099b2cc8f241bf5251caa763
Kareem Khaled <kareem.mohllal@gmail.com>
2022-05-26 17:13:51 +0200

    Time: 451 ms, Memory: 15 MB - LeetHub

---
+++ two-sum-ii-input-array-is-sorted/two-sum-ii-input-array-is-sorted.py
@@ -0,0 +1,46 @@
+class Solution:
+    # O(log n) time and O(log n) space
+    def binarySearchRecursive(self, left: int, right: int, numbers: List[int], target:int) -> int:
+        if left > right:
+            return -1
+        
+        mid = (left + right) // 2
+        
+        if numbers[mid] == target:
+            return mid
+        elif numbers[mid] > target:
+            return self.binarySearchRecursive(left, mid - 1, numbers, target)
+        else:
+            return self.binarySearchRecursive(mid + 1, right, numbers, target)
+    
+    # O(log n) time and O(1) space
+    def binarySearchIterative(self, left: int, right: int, numbers: List[int], target:int) -> int:
+        while left <= right:
+            mid = (left + right) // 2
+            
+            if numbers[mid] == target:
+                return mid
+            elif numbers[mid] > target:
+                right = mid - 1
+            else:
+                left = mid + 1
+        return -1
+
+    # O(n * log n) time and O(n * log n) space
+    def twoSumLinearithmicTimeAndLinearithmicSpace(self, numbers: List[int], target: int) -> List[int]:
+        for i in range(0, len(numbers)):
+            remaining = target - numbers[i]
+            j = self.binarySearchRecursive(i + 1, len(numbers) - 1, numbers, remaining)
+            if j != -1:
+                return [i + 1, j + 1]
+    
+    # O(n * log n) time and O(n) space
+    def twoSumLinearithmicTimeAndLinearSpace(self, numbers: List[int], target: int) -> List[int]:
+        for i in range(0, len(numbers)):
+            remaining = target - numbers[i]
+            j = self.binarySearchIterative(i + 1, len(numbers) - 1, numbers, remaining)
+            if j != -1:
+                return [i + 1, j + 1]
+
+    def twoSum(self, numbers: List[int], target: int) -> List[int]:
+        return self.twoSumLinearithmicTimeAndLinearSpace(numbers, target)
