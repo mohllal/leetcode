13cb1bd214d1ab9708188e5bdb1ef578d6cbe31e
Kareem Khaled <kareem.mohllal@gmail.com>
2023-01-12 02:00:32 +0200

    Time: 659 ms (96.18%), Space: 50.6 MB (81.53%) - LeetHub

--- 1443-minimum-time-to-collect-all-apples-in-a-tree/1443-minimum-time-to-collect-all-apples-in-a-tree.py
+++ 1443-minimum-time-to-collect-all-apples-in-a-tree/1443-minimum-time-to-collect-all-apples-in-a-tree.py
@@ -1,10 +1,3 @@
-class Node:
-    def __init__(self, hasApple, key):
-        self.left = None
-        self.right = None
-        self.hasApple = hasApple
-        self.key = key
-
 class Solution:
     def dfs(self, node, parent, adjacent, hasApple):
         if node not in adjacent:
@@ -14,13 +7,14 @@ class Solution:
         for child in adjacent[node]:
             if child == parent:
                 continue
-
+           
             childTime = self.dfs(child, node, adjacent, hasApple)
             if childTime > 0 or hasApple[child]:
                 totalTime += 2 + childTime
         
         return totalTime
 
+    # O(n) time and O(n) space
     def minTime(self, n: int, edges: List[List[int]], hasApple: List[bool]) -> int:
         adjacent: DefaultDict[int, []]= defaultdict(lambda: [])
         for edge in edges:
@@ -28,4 +22,3 @@ class Solution:
             adjacent[edge[1]].append(edge[0])
 
         return self.dfs(0, -1, adjacent, hasApple)
-        
