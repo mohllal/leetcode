fb0dc4e4b61f24944d7ade8ac54102f2a2ab8994
Kareem Khaled <kareem.mohllal@gmail.com>
2023-01-21 20:33:40 +0200

    Time: 48 ms (53.15%), Space: 14 MB (31.50%) - LeetHub

--- 0093-restore-ip-addresses/0093-restore-ip-addresses.py
+++ 0093-restore-ip-addresses/0093-restore-ip-addresses.py
@@ -1,37 +1,37 @@
 class Solution:
-    def insert_dot(self, string, index):
+    def insertDot(self, string, index):
         return string[:index] + '.' + string[index:]
 
     def isValidIP(self, s):
-        ip = s.replace('.', ' ').split()
-        
-        if len(ip) != 4:
+        divs = s.replace('.', ' ').split()
+
+        if len(divs) != 4:
             return False
-        
-        for div in ip:
+
+        for div in divs:
             if len(div) > 1 and div[0] == '0':
                 return False
-            
+
             if int(div) > 255:
                 return False
 
         return True
-            
+
     def backtrack(self, s, subsets):
         if s.count('.') == 3:
             if self.isValidIP(s):
                 subsets.add(s)
-            return     
+            return
         else:
             last = s.rfind('.')
-            
-            oneDigitDiv = self.insert_dot(s, last + 2)
+
+            oneDigitDiv = self.insertDot(s, last + 2)
             self.backtrack(oneDigitDiv, subsets)
-            
-            twoDigitDiv = self.insert_dot(s, last + 3)
+
+            twoDigitDiv = self.insertDot(s, last + 3)
             self.backtrack(twoDigitDiv, subsets)
 
-            threeDigitDiv = self.insert_dot(s, last + 4)
+            threeDigitDiv = self.insertDot(s, last + 4)
             self.backtrack(threeDigitDiv, subsets)
 
     def restoreIpAddresses(self, s: str) -> List[str]:
