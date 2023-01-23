fd0b5fb88644d9d87c42aba51b09f51c9cc56c33
Kareem Khaled <kareem.mohllal@gmail.com>
2023-01-24 00:39:21 +0200

    Time: 810 ms (46.61%), Space: 18.8 MB (99.48%) - LeetHub

--- 0997-find-the-town-judge/0997-find-the-town-judge.py
+++ 0997-find-the-town-judge/0997-find-the-town-judge.py
@@ -10,18 +10,33 @@ class Solution:
         trusters: DefaultDict[int, set] = defaultdict(lambda: set())
         trustees: DefaultDict[int, set] = defaultdict(lambda: set())
         
+        candidate = 0
+        maximum = 0
+        occurrence = 0
         for trust in trusts:
             trusters[trust[0]].add(trust[1])
             trustees[trust[1]].add(trust[0])
+            
+            if len(trustees[trust[1]]) == maximum:
+                occurrence += 1
+            elif len(trustees[trust[1]]) > maximum:
+                maximum = len(trustees[trust[1]])
+                occurrence = 1
+                candidate = trust[1]
         
-        result = -1
-        for a in trustees:
-            if len(trustees[a]) == n - 1 and len(trusters[a]) == 0:
-                if result == -1:
-                    result = a
-                else:
-                    result = -1
-                    break
+        if maximum == n - 1 and len(trusters[candidate]) == 0 and occurrence == 1:
+            return candidate
+        else:
+            return -1
         
-        return result
+#         result = -1
+#         for a in trustees:
+#             if len(trustees[a]) == n - 1 and len(trusters[a]) == 0:
+#                 if result == -1:
+#                     result = a
+#                 else:
+#                     result = -1
+#                     break
+        
+#         return result
             
