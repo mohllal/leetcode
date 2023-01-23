ff15cc366a5aa22233e97b4e28f765826542ef16
Kareem Khaled <kareem.mohllal@gmail.com>
2023-01-24 00:44:22 +0200

    Time: 2379 ms (5.21%), Space: 18.8 MB (90.40%) - LeetHub

--- 0997-find-the-town-judge/0997-find-the-town-judge.py
+++ 0997-find-the-town-judge/0997-find-the-town-judge.py
@@ -1,42 +1,33 @@
 class Solution:
     # O(n) time and O(n) space
     def findJudge(self, n: int, trusts: List[List[int]]) -> int:
-        if len(trusts) == 0 and n == 1:
-            return 1
-
-        if len(trusts) == 0 and n > 1:
+        if len(trusts) == 0:
+            if n > 1:
                 return -1
-
+            else:
+                return 1
+    
         trusters: DefaultDict[int, set] = defaultdict(lambda: set())
         trustees: DefaultDict[int, set] = defaultdict(lambda: set())
         
         candidate = 0
-        maximum = 0
-        occurrence = 0
+        candidateOccurrences = 0
+        maximumTrusts = 0
         for trust in trusts:
             trusters[trust[0]].add(trust[1])
             trustees[trust[1]].add(trust[0])
             
-            if len(trustees[trust[1]]) == maximum:
-                occurrence += 1
-            elif len(trustees[trust[1]]) > maximum:
-                maximum = len(trustees[trust[1]])
-                occurrence = 1
+            if len(trustees[trust[1]]) == maximumTrusts:
+                candidateOccurrences += 1
+
+            elif len(trustees[trust[1]]) > maximumTrusts:
+                maximumTrusts = len(trustees[trust[1]])
+                candidateOccurrences = 1
                 candidate = trust[1]
         
-        if maximum == n - 1 and len(trusters[candidate]) == 0 and occurrence == 1:
+        if (maximumTrusts == n - 1
+            and len(trusters[candidate]) == 0
+            and candidateOccurrences == 1):
             return candidate
         else:
             return -1
-        
-#         result = -1
-#         for a in trustees:
-#             if len(trustees[a]) == n - 1 and len(trusters[a]) == 0:
-#                 if result == -1:
-#                     result = a
-#                 else:
-#                     result = -1
-#                     break
-        
-#         return result
-            
