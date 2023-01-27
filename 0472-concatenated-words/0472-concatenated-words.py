b4656975da3d084abd7e7a52e8bc867bfef4d65a
Kareem Khaled <kareem.mohllal@gmail.com>
2023-01-28 01:44:26 +0200

    Time: 626 ms (50.19%), Space: 16.8 MB (79.60%) - LeetHub

---
+++ 0472-concatenated-words/0472-concatenated-words.py
@@ -0,0 +1,28 @@
+class Solution:
+    def dfs(self, word, length, visited, words):
+        if length == len(word):
+            return True
+
+        if visited[length]:
+            return False
+
+        visited[length] = True
+        for i in range(len(word) - (1 if length == 0 else 0), length, -1):
+            if (
+                word[length:i] in words and
+                self.dfs(word, i, visited, words)
+               ):
+                return True
+        return False
+
+    def findAllConcatenatedWordsInADict(self, words: List[str]) -> List[str]:
+        wordsSet = set(words)
+        answer = []
+
+        for word in words:
+            length = len(word)
+            visited = [False] * length
+            if self.dfs(word, 0, visited, wordsSet):
+                answer.append(word)
+
+        return answer
