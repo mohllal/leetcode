991c1a18844219fea2505d268c7c1ecbcf0e4515
Kareem Khaled <kareem.mohllal@gmail.com>
2023-02-03 02:01:36 +0200

    Time: 43 ms (49.47%), Space: 13.9 MB (31.45%) - LeetHub

--- 0953-verifying-an-alien-dictionary/0953-verifying-an-alien-dictionary.py
+++ 0953-verifying-an-alien-dictionary/0953-verifying-an-alien-dictionary.py
@@ -1,23 +1,18 @@
 class Solution:
-    # O(m) time and O(1) space
-    def wordOrder(self, word, orders):
-        order = 0
-        for char in word:
-            order += orders[char]
-        
-        return order
-
     # O(n * m) time and O(1) space
     def isAlienSorted(self, words: List[str], order: str) -> bool:
         orders = {order[i]: i + 1 for i in range(len(order))}
 
         for i in range(len(words) - 1):
-            for j in range(len(words[i])):
-                if j >= len(words[i + 1]):
+            currentWord = words[i]
+            nextWord = words[i + 1]
+    
+            for j in range(len(currentWord)):
+                if j >= len(nextWord):
                     return False
                 
-                if words[i][j] != words[i + 1][j]:
-                    if orders[words[i][j]] > orders[words[i + 1][j]]:
+                if currentWord[j] != nextWord[j]:
+                    if orders[currentWord[j]] > orders[nextWord[j]]:
                         return False
                     break
         return True
