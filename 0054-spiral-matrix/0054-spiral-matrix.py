8233f109911d883c17de708a03413f04eef47b55
Kareem Khaled <kareem.mohllal@gmail.com>
2023-09-10 23:20:34 +0300

    Time: 38 ms (66.23%), Space: 16.3 MB (45.74%) - LeetHub

---
+++ 0054-spiral-matrix/0054-spiral-matrix.py
@@ -0,0 +1,36 @@
+class Solution:
+    # O(n) time and O(n) space - n is the total number of elements
+    def spiralOrder(self, matrix: List[List[int]]) -> List[int]:
+        output = []
+        rows = len(matrix)
+        columns = len(matrix[0])
+
+        rStart, rEnd = 0, rows - 1
+        cStart, cEnd = 0, columns - 1
+        while rStart <= rEnd and cStart <= cEnd:
+            self.rectangleTraverse(rStart, rEnd, cStart, cEnd, matrix, output)
+
+            rStart += 1
+            rEnd -= 1
+
+            cStart += 1
+            cEnd -= 1
+
+        return output
+    
+    def rectangleTraverse(self, rStart, rEnd, cStart, cEnd, matrix, output):
+        for column in range(cStart, cEnd + 1):
+            output.append(matrix[rStart][column])
+
+        for row in range(rStart + 1, rEnd + 1):
+            output.append(matrix[row][cEnd])
+
+        for column in reversed(range(cStart, cEnd)):
+            if rEnd == rStart:
+                break
+            output.append(matrix[rEnd][column])
+
+        for row in reversed(range(rStart + 1, rEnd)):
+            if cEnd == cStart:
+                break
+            output.append(matrix[row][cStart])
