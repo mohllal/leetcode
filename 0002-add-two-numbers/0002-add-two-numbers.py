068a49fb08c7a5ab6699fea55ca41b50a3c7376a
Kareem Khaled <kareem.mohllal@gmail.com>
2023-09-22 02:44:33 +0300

    Time: 72 ms (24.31%), Space: 16.5 MB (21.55%) - LeetHub

--- 0002-add-two-numbers/0002-add-two-numbers.py
+++ 0002-add-two-numbers/0002-add-two-numbers.py
@@ -4,8 +4,31 @@ class ListNode:
         self.next = next
 
 class Solution:
-    # O(n+m) time and O(n+m) space
     def addTwoNumbers(self, l1: Optional[ListNode], l2: Optional[ListNode]) -> Optional[ListNode]:
+        return self.addTwoNumbersFasterLinearTimeAndLinearSpace(l1, l2)
+
+    # O(n+m) time and O(n+m) space
+    def addTwoNumbersFasterLinearTimeAndLinearSpace(self, l1, l2):
+        return self.sumLists(l1, l2)
+
+    def sumLists(self, l1, l2, carry=0):
+        if l1 is None and l2 is None and carry == 0:
+            return None
+        
+        l1Val = l1.val if l1 is not None else 0
+        l2Val = l2.val if l2 is not None else 0
+        lSum = l1Val + l2Val + carry
+        
+        l1Next = l1.next if l1 is not None else None
+        l2Next = l2.next if l2 is not None else None
+    
+        node = ListNode(lSum % 10)
+        node.next = self.sumLists(l1Next, l2Next, lSum // 10)
+    
+        return node
+        
+    # O(n+m) time and O(n+m) space
+    def addTwoNumbersLinearTimeAndLinearSpace(self, l1, l2):
         l1Number = self.listToNumber(l1)
         l2Number = self.listToNumber(l2)
         
