ec9860b50146d57ff8429e13867f0ddb2a8d62d1
Kareem Khaled <kareem.mohllal@gmail.com>
2023-09-22 01:56:15 +0300

    Time: 40 ms (64.26%), Space: 16.3 MB (19.54%) - LeetHub

--- 0509-fibonacci-number/0509-fibonacci-number.py
+++ 0509-fibonacci-number/0509-fibonacci-number.py
@@ -1,6 +1,6 @@
 class Solution:
     def fib(self, n: int) -> int:
-        return self.fibLinearTimeAndLinearSpace(n)
+        return self.fibLinearTimeAndConstantSpace(n)
     
     # O(2^n) time and O(n) space
     def fibExponentialTimeAndLinearSpace(self, n: int) -> int:
@@ -25,3 +25,14 @@ class Solution:
             return memo[n]
         
         return fibHelper(n)
+    
+    # O(n) time and O(1) space
+    def fibLinearTimeAndConstantSpace(self, n: int) -> int:
+        fib = [0, 1]
+        
+        for i in range(2, n + 1):
+            current = fib[0] + fib[1]
+            fib[0] = fib[1]
+            fib[1] = current
+        
+        return fib[n] if n <= 1 else fib[1]
