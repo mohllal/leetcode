7751435bd854328f75f394b4841f8fbc4c5850b5
Kareem Khaled <kareem.mohllal@gmail.com>
2023-09-26 01:04:32 +0300

    Time: 48 ms (30.21%), Space: 16.4 MB (18.06%) - LeetHub

--- 0093-restore-ip-addresses/0093-restore-ip-addresses.py
+++ 0093-restore-ip-addresses/0093-restore-ip-addresses.py
@@ -1,6 +1,22 @@
 class Solution:
-    def isValidIPAddressPart(self, part):
-        return part and int(part) <= 255 and len(part) == len(str(int(part)))
+    def isValidIPAddressSegment(self, segment):
+        return segment and int(segment) <= 255 and len(segment) == len(str(int(segment)))
+
+    # O(1) time and O(1) space
+    def restoreIpAddressesRecursive(self, string, start, currentIp, currentSegment, ipAddresses):
+        if currentSegment == 4:
+            if start == len(string):
+                ipAddresses.append(currentIp)
+            return
+
+        for i in range(1, min(4, len(string) - start + 1)):
+            segment = string[start:start+i]
+            
+            if self.isValidIPAddressSegment(segment):
+                newIp = currentIp + segment if currentSegment == 3 else currentIp + segment + '.'
+                self.restoreIpAddressesRecursive(string, start + i, newIp, currentSegment + 1, ipAddresses)
+
+        return ipAddresses
 
     # O(1) time and O(1) space
     def restoreIpAddressesIterative(self, s):
@@ -9,21 +25,26 @@ class Solution:
         for i in range(1, 4):
             for j in range(1, 4):
                 for k in range(1, 4):
-                    firstPart = s[:i]
-                    secondPart = s[i:i+j]
-                    thirdPart = s[i+j:i+j+k]
-                    fourthPart = s[i+j+k:]
+                    firstSegment = s[:i]
+                    secondSegment = s[i:i+j]
+                    thirdSegment = s[i+j:i+j+k]
+                    fourthSegment = s[i+j+k:]
 
                     if (
-                        self.isValidIPAddressPart(firstPart) and
-                        self.isValidIPAddressPart(secondPart) and
-                        self.isValidIPAddressPart(thirdPart) and
-                        self.isValidIPAddressPart(fourthPart)
+                        self.isValidIPAddressSegment(firstSegment) and
+                        self.isValidIPAddressSegment(secondSegment) and
+                        self.isValidIPAddressSegment(thirdSegment) and
+                        self.isValidIPAddressSegment(fourthSegment)
                     ):
-                        ipAddress = ".".join([firstPart, secondPart, thirdPart, fourthPart])
+                        ipAddress = ".".join([
+                            firstSegment,
+                            secondSegment,
+                            thirdSegment,
+                            fourthSegment
+                        ])
                         ipAddresses.append(ipAddress)                
 
         return ipAddresses
 
     def restoreIpAddresses(self, s: str) -> List[str]:
-        return self.restoreIpAddressesIterative(s)
+        return self.restoreIpAddressesRecursive(s, 0, '', 0, [])
