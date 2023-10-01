a802e8e93b29b01257ef8915c1d711c3ff719ed9
Kareem Khaled <kareem.mohllal@gmail.com>
2023-10-01 23:18:21 +0300

    Time: 51 ms (92.95%), Space: 16.3 MB (99.72%) - LeetHub

--- 0047-permutations-ii/0047-permutations-ii.py
+++ 0047-permutations-ii/0047-permutations-ii.py
@@ -1,6 +1,5 @@
 class Solution:
-    # O(Σk=1, n P(n,k)) time and O(n*n!) space
-    # P(n,k) = n!/(n-k)!
+    # O(n*n!) time and O(n*n!) space
     def permuteUnique(self, nums: List[int]) -> List[List[int]]:
         def permuteHelper(nums, prefix, visited, premutations):
             if len(prefix) == len(nums):
