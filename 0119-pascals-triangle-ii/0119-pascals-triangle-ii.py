d467678ed680d75610ce943319956f73129841e7
Kareem Khaled <kareem.mohllal@gmail.com>
2023-11-02 00:31:20 +0200

    Time: 41 ms (38.65%), Space: 16.1 MB (95.12%) - LeetHub

---
+++ 0119-pascals-triangle-ii/0119-pascals-triangle-ii.py
@@ -0,0 +1,16 @@
+class Solution:
+    # O(n^2) time and O(n) space
+    def getRow(self, rowIndex: int) -> List[int]:
+        if rowIndex == 0:
+            return [1]
+
+        prevRow = self.getRow(rowIndex - 1)
+        currRow = [1] * (rowIndex + 1)
+
+        for i in range(1, len(currRow) - 1):
+            currRow[i] = prevRow[i - 1] + prevRow[i]
+
+        return currRow
+            
+            
+            
