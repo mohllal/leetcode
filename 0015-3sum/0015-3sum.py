d584c9d530c73a98df46b7ecb9ea83c4549e5888
Kareem Khaled <kareem.mohllal@gmail.com>
2024-08-24 01:57:23 +0300

    Time: 1378 ms (16.41%), Space: 20.4 MB (89.97%) - LeetHub

--- 0015-3sum/0015-3sum.py
+++ 0015-3sum/0015-3sum.py
@@ -5,15 +5,17 @@ class Solution:
         triples = set()
 
         for i in range(len(nums)):
+            target = -nums[i]
+    
             j = i + 1
             k = len(nums) - 1
-            
+
             while j < k:
-                current = nums[i] + nums[j] + nums[k]
+                current = nums[j] + nums[k]
                 
-                if current < 0:
+                if current < target:
                     j += 1
-                elif current > 0:
+                elif current > target:
                     k -= 1
                 else:
                     triples.add((nums[i], nums[j], nums[k]))
