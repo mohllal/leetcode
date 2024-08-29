56100f75ba9f728a74df76f4e646956890bd69cb
Kareem Khaled <kareem.mohllal@gmail.com>
2024-08-29 15:44:00 +0300

    Time: 33 ms (81.99%), Space: 16.6 MB (22.76%) - LeetHub

--- 0075-sort-colors/0075-sort-colors.py
+++ 0075-sort-colors/0075-sort-colors.py
@@ -1,6 +1,6 @@
 class Solution:
     # O(n) time and O(1) space
-    def sortColors(self, nums: List[int]) -> None:
+    def sortColorsLinearTimeAndConstantSpace1(self, nums: List[int]) -> None:
         last_red = 0
         for i in range(len(nums)):
             if nums[i] == 0:
@@ -14,3 +14,25 @@ class Solution:
                 last_blue -= 1
      
         return nums
+    
+    # O(n) time and O(1) space
+    def sortColorsLinearTimeAndConstantSpace2(self, nums: List[int]) -> None:
+        red = 0
+        blue = len(nums) - 1
+        
+        current = 0
+        while current <= blue:
+            if nums[current] == 0:
+                nums[current], nums[red] = nums[red], nums[current]
+                current += 1
+                red += 1
+            elif nums[current] == 2:
+                nums[current], nums[blue] = nums[blue], nums[current]
+                blue -= 1
+            else:
+                current += 1
+                
+        return nums
+
+    def sortColors(self, nums: List[int]) -> None:
+        return self.sortColorsLinearTimeAndConstantSpace2(nums)
