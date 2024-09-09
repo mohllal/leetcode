fd133c85b545307ef00aa1e1f88e74e374a23d87
Kareem Khaled <kareem.mohllal@gmail.com>
2024-09-09 14:59:32 +0300

    Time: 495 ms (5.97%), Space: 17.1 MB (72.69%) - LeetHub

--- 1004-max-consecutive-ones-iii/1004-max-consecutive-ones-iii.py
+++ 1004-max-consecutive-ones-iii/1004-max-consecutive-ones-iii.py
@@ -4,14 +4,17 @@ class Solution:
         longest_length = 0
         
         window_start = 0
-        window_counter = Counter()
+        window_ones_count = 0
         for window_end in range(len(nums)):
             window_end_elem = nums[window_end]
-            window_counter[window_end_elem] += 1
+            if window_end_elem == 1:
+                window_ones_count += 1
 
-            while (window_end - window_start + 1) - window_counter[1] > k:
+            while (window_end - window_start + 1) - window_ones_count > k:
                 window_start_elem = nums[window_start]
-                window_counter[window_start_elem] -= 1
+                if window_start_elem == 1:
+                    window_ones_count -= 1
+    
                 window_start += 1
             
             longest_length = max(longest_length, (window_end - window_start) + 1)
